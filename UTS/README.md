967db687263be7ce2f83d4b0a344f45ba51cc40d
Agus Prayogi <agus21apy@gmail.com>
2023-10-26 08:56:54 +0700

    [ADD] uts

---
+++ UTS/README.md
@@ -0,0 +1,6 @@
+# UTS
+
+## Tugas Praktikum 1
+Penjelasan dan code program terdapat di dalam folder `basic_layout_flutter`.
+
+Link : [basic_layout_flutter](./basic_layout_flutter)
--- UTS/basic_layout_flutter/lib/main.dart
+++ UTS/basic_layout_flutter/lib/main.dart
@@ -29,58 +29,62 @@ class MyWidget extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Basic Layout'),
+        backgroundColor: Colors.blue,
       ),
-      body: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Image.network(
-            "https://picsum.photos/250?image=9",
-            width: 200,
-            height: 200,
-          ),
-          Row(
-            children: [
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Icon(Icons.account_circle, size: 50),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    'Flutter McFlutter',
-                    style: Theme.of(context).textTheme.headlineSmall,
-                  ),
-                  const Text('Experienced App Developer'),
-                ],
-              ),
-            ],
-          ),
-          const SizedBox(height: 8),
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                '123 Main Street',
-              ),
-              Text(
-                '(415) 555-0198',
-              ),
-            ],
-          ),
-          const SizedBox(height: 16),
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Icon(Icons.accessibility),
-              Icon(Icons.timer),
-              Icon(Icons.phone_android),
-              Icon(Icons.phone_iphone),
-            ],
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.network(
+              "https://picsum.photos/250?image=9",
+              width: 200,
+              height: 200,
+            ),
+            Row(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Icon(Icons.account_circle, size: 50),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Flutter McFlutter',
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    ),
+                    const Text('Experienced App Developer'),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(height: 8),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '123 Main Street',
+                ),
+                Text(
+                  '(415) 555-0198',
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Icon(Icons.accessibility),
+                Icon(Icons.timer),
+                Icon(Icons.phone_android),
+                Icon(Icons.phone_iphone),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
